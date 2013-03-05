8f4bc9e43ca0fa05b86f821b8e49229ab0e4a911
Péter Karakas <karakas.peter@gmail.com>
2013-03-05 20:08:20 +0100

    Programozási feladatot listázza, meg lehet oldani és el is küldi a megoldást. Amit még javítani kell: - Quiz végét ki lehessen választani - A megoldás eredményét nem menti le adatbázisba

---
+++ protected/config/params.php
@@ -0,0 +1,7 @@
+<?php
+
+// this contains the application parameters that can be maintained via GUI
+return array(
+    //Megoldógép adatok, a többi app-ban is át kell írni
+    'eveluateIP' => '192.168.1.115',
+);
